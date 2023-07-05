db7adc469a95fe2a13809f4d6565cfd5026bf890
William de Souza <william.sza@outlook.com>
2023-07-05 16:57:10 -0300

    fist commit

---
+++ crud-rest-api-springboot-com-docker-e-postgresql/src/test/java/restapispringbootcomdockerepostgresql/CrudRestApiSpringbootComDockerEPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package restapispringbootcomdockerepostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestApiSpringbootComDockerEPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
